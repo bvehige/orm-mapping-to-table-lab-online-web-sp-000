21b43fde91fe6e61afd617b68e28e2b924b6f39f
Brian Vehige <brianvehige@gmail.com>
2020-04-10 06:17:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,20 +13,23 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-      if INTEGER PRIMARY KEY,
+      id INTEGER PRIMARY KEY,
       name TEXT,
-      grade INTEGER);
+      grade INTEGER)
+      SQL
+      DB[:conn].execute(sql)
+    end
 
-
-def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        album TEXT
-        )
-        SQL
-    DB[:conn].execute(sql) 
-  end  
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
   
+  def save
+    
+  
+    
+
 end
